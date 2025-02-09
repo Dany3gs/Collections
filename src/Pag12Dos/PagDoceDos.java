ff234b3110cb4168b13f449ac479b87983076e33
Dany3gs <dsmmgc@gmail.com>
2025-02-09 20:45:10 +0000

    Ejercicio Pag12Dos, explicado con System.nanoTime()

--- src/Pag12Dos/PagDoceDos.java
+++ src/Pag12Dos/PagDoceDos.java
@@ -6,5 +6,40 @@ Desarrolla un programa que inserte 1000 registros en un ArrayList y en un Linked
 Posteriormente, calcule el tiempo en que tarda cada una de las listas en insertar un valor en la posición 100;
 */
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class PagDoceDos {
+	public static void main(String[] args) {
+		
+		// Una forma visual en terminal es usando la clase System.nanoTime(), declarados antes y después del código
+		// A medir desde --------------------------------
+		long startTime1 = System.nanoTime();
+		ArrayList listaArray = new ArrayList();
+		
+		for (int i = 0; i < 1000; i++) {
+			listaArray.add(i);
+		}
+		System.out.println(listaArray.get(100));
+		long endTime1 = System.nanoTime();
+		// A medir hasta --------------------------------
+		
+		System.out.println("Tiempo de ejecución del ArrayList: " + (endTime1 - startTime1) + " nanosegundos");
+		System.out.println("-------------------------------------");
+		
+		// A medir desde --------------------------------
+		long startTime2 = System.nanoTime();
+		LinkedList listaLinkedList = new LinkedList();
+		for (int i = 0; i < 1000; i++) {
+			listaLinkedList.add(i);
+		}
+		System.out.println(listaLinkedList.get(100));
+		long endTime2 = System.nanoTime();
+		// A medir hasta --------------------------------
+		
+		System.out.println("Tiempo de ejecución del LinkedList: " + (endTime2 - startTime2) + " nanosegundos");
+		System.out.println("-------------------------------------");
+		System.out.println("La diferencia de tiempo de ejecución es de: " + ((endTime1 - startTime1) - (endTime2 - startTime2)) + " nanosegundos");
+		System.out.println("Indicando que el LinkedList es más lento que el ArrayList en esta operación.");
+	}
 }
